6d815e1eaacd64e1b38adcad06c154d77fdf3335
Rusheel Dasari <rusheel@gmail.com>
2023-04-27 15:58:53 -0500

    Added some starting variables and methods to tetris.c and tetris.h

--- FinalProject/tetris.c
+++ FinalProject/tetris.c
@@ -5,6 +5,113 @@
  *     Authors: Rusheel Dasari and Khiem Vu
  */
 
+#include "tetris.h"
 
+void generate_next() {
+    PieceType type = (PieceType)(rand() % 7 + 1));
+    next->type = type;
+    next->x = -1;
+    next->y = -1;
+    next->right_max = -1;
+    next->left_max = -1;
+    next->bottom_max = -1;
 
+    switch (next->type) {
+    case LINE:
+        Block block1;
+        Block block2;
+        Block block3;
+        Block block4;
+        next->blocks;
+        break;
+    case SQUARE:
+        Block block1;
+        Block block2;
+        Block block3;
+        Block block4;
+        next->blocks;
+        break;
+    case J:
+        Block block1;
+        Block block2;
+        Block block3;
+        Block block4;
+        next->blocks;
+        break;
+    case L:
+        Block block1;
+        Block block2;
+        Block block3;
+        Block block4;
+        next->blocks;
+        break;
+    case S:
+        Block block1;
+        Block block2;
+        Block block3;
+        Block block4;
+        next->blocks;
+        break;
+    case Z:
+        Block block1;
+        Block block2;
+        Block block3;
+        Block block4;
+        next->blocks;
+        break;
+    case T:
+        Block block1;
+        Block block2;
+        Block block3;
+        Block block4;
+        next->blocks;
+        break;
+    default:
+        Block block1;
+        Block block2;
+        Block block3;
+        Block block4;
+        next->blocks;
+        break;
+    }
+}
 
+void init_board() {
+    int row, col;
+
+    board = malloc(sizeof(Block*) * GRID_HEIGHT);
+    if (board == NULL) {
+        exit(1);
+    }
+
+    for(row = 0; row < GRID_HEIGHT; row++) {
+        *(board + row) = malloc(sizeof(Block) * GRID_WIDTH);
+        if ((*(board + row)) == NULL) {
+            exit(1);
+        }
+    }
+
+    for(row = 0; row < GRID_HEIGHT; row++) {
+        for(col = 0; col < GRID_WIDTH; col++) {
+            board[row][col]->empty = true;
+            board[row][col]->linked = false;
+            board[row][col]->x = col;
+            board[row][col]->y = row;
+            board[row][col]->x_offset = 0;
+            board[row][col]->y_offset = 0;
+            board[row][col]->axis = false;
+            board[row][col]->type = EMPTY;
+        }
+    }
+}
+
+void init_game() {
+    init_board();
+    current = malloc(sizeof(Block));
+    next = malloc(sizeof(Block));
+    held = malloc(sizeof(Block));
+
+    if(current == NULL || next == NULL || held == NULL) {
+        exit(1);
+    }
+}
--- FinalProject/tetris.h
+++ FinalProject/tetris.h
@@ -8,8 +8,50 @@
 #ifndef TETRIS_H_
 #define TETRIS_H_
 
+#include <stdbool.h>
+#include <stdlib.h>
 
+#define GRID_WIDTH      10
+#define GRID_HEIGHT     24
 
+enum PieceType {EMPTY, LINE, SQUARE, J, L, S, Z, T} typedef PieceType;
 
+struct Block {
+    bool empty;
+    bool linked;
+    int x;
+    int y;
+    int x_offset;
+    int y_offset;
+    bool axis;
+    PieceType type;
+} typedef Block;
+
+Block** board;
+
+struct Piece {
+    int x;
+    int y;
+    int left_max;
+    int right_max;
+    int bottom_max;
+    char rotation;
+    Block blocks[4];
+    PieceType type;
+} typedef Piece;
+
+Piece* current;
+Piece* next;
+Piece* held;
+
+void generate_next(void);
+void init_board(void);
+void init_game(void);
+void rotate_current_right(void);
+void rotate_current_left(void);
+void rotate_current_flip(void);
+void swap_held(void);
+void land_current(void);
+void clear_lines(void);
 
 #endif /* TETRIS_H_ */
