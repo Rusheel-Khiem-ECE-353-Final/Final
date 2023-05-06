0d680c36ddc2e4c6192dc5106eb209c25a0fa237
Khiem Vu <kmvu2@wisc.edu>
2023-05-05 22:22:25 -0500

    images

--- FinalProject/images.h
+++ FinalProject/images.h
@@ -8,4 +8,14 @@
 #ifndef IMAGES_H_
 #define IMAGES_H_
 
+#include <stdint.h>
+
+#define TITLE_WIDTH    110
+#define TITLE_HEIGHT   76
+extern const uint8_t Bitmaps_Title[];
+
+#define END_WIDTH     132
+#define END_HEIGHT    102
+extern const uint8_t Bitmaps_GameOver[];
+
 #endif /* IMAGES_H_ */
